10276cc62591fb7a02a98c0307db9b901aec0d0d
Gurrehmat Oberoi <gurrehmat_oberoi@external.mckinsey.com>
2019-02-06 00:42:36 +0530

    Added ratings to books

--- src/utils/books/index.js
+++ src/utils/books/index.js
@@ -1,7 +1,35 @@
 const { httpGet } = require('../http');
 
-const getAllBooksFromApi = () => httpGet('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks').then(response => response.data.books);
+const getAllBooksFromApi = () => {
+  const url = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks';
+  return httpGet(url).then(response => response.data.books);
+};
+
+const getBookRatingById = (id) => {
+  const url = `https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${id}`;
+  return httpGet(url).then(response => response.data);
+};
+
+const getBooksWithRating = () => getAllBooksFromApi().then((books) => {
+  const allBooks = books;
+  const allBookIds = allBooks.map(book => book.id);
+  const allPromises = allBookIds.map(id => getBookRatingById(id));
+  return Promise.all(allPromises).then(allRatings => allRatings.map((ratingObject, index) => Object.assign(allBooks[index], ratingObject)));
+});
+  // const allBooks = await getAllBooksFromApi();
+  // // const allPromises = [];
+  // const allPromises = allBooks.map((book) => {
+  //   const bookRating = getBookRatingById(book.id);
+  //   return bookRating;
+  // });
+  // const allRatings = Promise.all(allPromises).then();
+  // const allBooksWithRatings =
+  // allBooks.map((book, index) => Object.assign(book, allRatings[index]));
+  // return allBooksWithRatings;
+
 
 module.exports = {
   getAllBooksFromApi,
+  getBookRatingById,
+  getBooksWithRating,
 };
--- tests/utils/books/index.test.js
+++ tests/utils/books/index.test.js
@@ -2,12 +2,32 @@
  * @jest-environment node
  */
 
-const { getAllBooksFromApi } = require('../../../src/utils/books');
+const {
+  getAllBooksFromApi,
+  getBookRatingById,
+  getBooksWithRating,
+} = require('../../../src/utils/books');
 
 describe('getAllBooksFromApi', () => {
   it('should fetch all books from API', async () => {
     const allBooks = await getAllBooksFromApi();
-    console.log(allBooks);
+    // console.log(allBooks);
     expect(Array.isArray(allBooks)).toEqual(true);
+  });
+});
+
+describe('getBookRatingById', () => {
+  it('should return rating object for given ID', async () => {
+    const ratingObject = await getBookRatingById(10);
+    console.log(ratingObject);
+    expect(typeof (ratingObject.rating)).toEqual('number');
+  });
+});
+
+describe('getBooksWithRating', () => {
+  it('should return books with ratings added', async () => {
+    const allBooks = await getBooksWithRating();
+    console.log(allBooks);
+    expect(typeof (allBooks[0].rating)).toEqual('number');
   });
 });
